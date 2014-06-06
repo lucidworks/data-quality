6eec886f028f7f3ece8d06c1ee057c6c36576f4b
Mark Bennett <mark.bennett@lucidworks.com>
2014-06-06 13:58:23 -0700

    Support for File IO and some code cleanup

--- src/main/java/com/lucidworks/dq/diff/DiffIds.java
+++ src/main/java/com/lucidworks/dq/diff/DiffIds.java
@@ -1,7 +1,18 @@
 package com.lucidworks.dq.diff;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CodingErrorAction;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -44,57 +55,24 @@ public class DiffIds /*implements HasDescription*/ {
     return HELP_WHAT_IS_IT;
   }
   
+  public static String NL = System.getProperty("line.separator");
+
+  // command line options
   static Options options;
 
-  // Constant fields at the bottom, after main
-  
-  // Server Based
-  // Since we re-query, we allow it to be destructive
-  public static Set<String> idsAOnly_fromServers( HttpSolrServer serverA, HttpSolrServer serverB ) throws SolrServerException {
-	Set<String> idsA = SolrUtils.getAllIds( serverA );
-	Set<String> idsB = SolrUtils.getAllIds( serverB );
-	return SetUtils.inAOnly_destructive( idsA, idsB );
-  }
-  public static Set<String> idsBOnly_fromServers( HttpSolrServer serverA, HttpSolrServer serverB ) throws SolrServerException {
-	return idsAOnly_fromServers( serverB, serverA );
-  }
-  public static Set<String> idsIntersection_fromServers( HttpSolrServer serverA, HttpSolrServer serverB ) throws SolrServerException {
-    Set<String> idsA = SolrUtils.getAllIds( serverA );
-	Set<String> idsB = SolrUtils.getAllIds( serverB );
-	return SetUtils.intersection_destructive( idsA, idsB );
-  }
-  public static Set<String> idsUnion_fromServers( HttpSolrServer serverA, HttpSolrServer serverB ) throws SolrServerException {
-	Set<String> idsA = SolrUtils.getAllIds( serverA );
-	Set<String> idsB = SolrUtils.getAllIds( serverB );
-	return SetUtils.union_destructive( idsA, idsB );
-  }
-  
-  
-  // Higher Level
-
-  public static void diffIds( HttpSolrServer serverA, HttpSolrServer serverB,
-		  Set<String> optAOnly, Set<String> optBOnly,
-		  Set<String> optInBoth, Set<String> optUnion
-  ) throws SolrServerException {
-	  // TODO: could optimize if only looking for 1 of them
-	  Set<String> idsA = SolrUtils.getAllIds( serverA );
-	  Set<String> idsB = SolrUtils.getAllIds( serverB );
-	  if ( null!=optAOnly ) {
-		  optAOnly.addAll( idsA );
-		  optAOnly.removeAll( idsB );
-	  }
-	  if ( null!=optBOnly ) {
-		  optBOnly.addAll( idsB );
-		  optBOnly.removeAll( idsA );
-	  }
-	  if ( null!=optInBoth ) {
-		  optInBoth.addAll( idsA );
-		  optInBoth.retainAll( idsB );
-	  }
-	  if ( null!=optUnion ) {
-		  optUnion.addAll( idsA );
-		  optUnion.addAll( idsB );
-	  }
+  static Set<String> readIdsFromFile( File targetFile, CharsetDecoder deccoder ) throws IOException {
+    Set<String> ids = new LinkedHashSet<String>();
+    BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(targetFile), deccoder));
+    String line;
+    while ((line = in.readLine()) != null) {
+      // skip completely blank lines, but doesn't do any trimming
+      if ( line.length()<1 ) {
+    	continue;
+      }
+      ids.add( line );
+    }
+    in.close();
+    return ids;
   }
 
   static void helpAndExit() {
@@ -114,21 +92,12 @@ public class DiffIds /*implements HasDescription*/ {
 	//formatter.printHelp( HELP_USAGE, options, true );
     // stderr
     PrintWriter pw = new PrintWriter(System.err);
-	formatter.printHelp( pw, 78, HELP_USAGE, null, options, 1, 0, null, true );
+	formatter.printHelp( pw, 78, HELP_USAGE, null, options, 1, 1, null, true );
 	pw.flush();
 	System.exit( errorCode );
   }
 
-  public static void main( String[] argv ) throws SolrServerException {
-    /**
-	System.out.println( "Getting IDs from "+URL0 + " ..." );
-    HttpSolrServer s0 = new HttpSolrServer( URL0 );
-    long start = System.currentTimeMillis();
-    Set<String> ids = getIds( s0 );
-    long end = System.currentTimeMillis();
-    long duration = end - start;
-    System.out.println( "Got " + ids.size() + " docs in " + duration + " ms" );
-    **/
+  public static void main( String[] argv ) throws SolrServerException, IOException {
 
     options = new Options();
 	options.addOption( "u", "url_a", true, "URL for first Solr, Eg http://localhost:8983/solr/collection1, OR set host, port and possibly collection" );
@@ -141,6 +110,13 @@ public class DiffIds /*implements HasDescription*/ {
 	options.addOption( "P", "port_b", true, "Port for second Solr, default=8983" );
 	options.addOption( "C", "collection_b", true, "Collection/Core for second Solr, Eg: collection1" );
 
+	options.addOption( "f", "file_a", true, "Read IDs for A from a text file (one ID per line, assumes UTF-8 encoding)" );
+	options.addOption( "F", "file_b", true, "Read IDs for B from a text file (one ID per line, assumes UTF-8 encoding)" );
+
+	options.addOption( "o", "output_file", true, "Output file to create for the full report or ID list (default or \"-\" is stdout / standard out)" );
+	options.addOption( "e", "encoding", true, "Character Encoding for reading and writing files (default is UTF-8, which enables cross-platform comparisons)" );
+	options.addOption( "l", "use_loose_encoding", false, "Disable strict character encoding so that problems don't throw Exceptions (NOT recommended)" );
+
 	options.addOption( "m", "mode", true,
 			"What to output:"
 			+ " \"" + MODE_REPORT + "\" means fully formatted report (default)"
@@ -148,7 +124,6 @@ public class DiffIds /*implements HasDescription*/ {
 			+ ", \"" + MODE_B_ONLY + "\" IDs only in B"
 			+ ", \"" + MODE_INTERSECT + "\" IDs preent in BOTH A AND B"
 			+ ", \"" + MODE_UNION + "\" IDs in A or B or in both (combines all IDs from both, but each ID will only appear once)"
-			+ "; all modes send their output to stdout / standard out (which you can rediret to a file with \" > output.txt\")"
 			);	
     if ( argv.length < 1 ) {
       helpAndExit();
@@ -171,22 +146,32 @@ public class DiffIds /*implements HasDescription*/ {
     String hostA = cmd.getOptionValue( "host_a" );
     String portA = cmd.getOptionValue( "port_a" );
     String collA = cmd.getOptionValue( "collection_a" );
-    if ( null==fullUrlA && null==hostA ) {
-      helpAndExit( "Must specifify at least url or host for first Solr", 3 );
+    String fileA = cmd.getOptionValue( "file_a" );
+    int optsA = 0;
+    optsA += (null!=fullUrlA) ? 1 : 0;
+    optsA += (null!=hostA) ? 1 : 0;
+    optsA += (null!=fileA) ? 1 : 0;
+    if ( optsA < 1 ) {
+      helpAndExit( "Must specifify at least url or host or ids file for first Solr instance", 3 );
     }
-    if ( null!=fullUrlA && null!=hostA ) {
-      helpAndExit( "Must not specifify both url and host for first Solr", 4 );
+    if ( optsA > 1 ) {
+      helpAndExit( "Can only specifify one of url, host or ids file for first Solr instance", 4 );
     }
 
     String fullUrlB = cmd.getOptionValue( "url_b" );
     String hostB = cmd.getOptionValue( "host_b" );
     String portB = cmd.getOptionValue( "port_b" );
     String collB = cmd.getOptionValue( "collection_b" );
-    if ( null==fullUrlB && null==hostB ) {
-      helpAndExit( "Must specifify at least url or host for second Solr", 3 );
+    String fileB = cmd.getOptionValue( "file_b" );
+    int optsB = 0;
+    optsB += (null!=fullUrlB) ? 1 : 0;
+    optsB += (null!=hostB) ? 1 : 0;
+    optsB += (null!=fileB) ? 1 : 0;
+    if ( optsB < 1 ) {
+      helpAndExit( "Must specifify at least url or host or ids file for second Solr instance", 3 );
     }
-    if ( null!=fullUrlB && null!=hostB ) {
-      helpAndExit( "Must not specifify both url and host for second Solr", 4 );
+    if ( optsB > 1 ) {
+      helpAndExit( "Can only specifify one of url, host or ids file for second Solr instance", 4 );
     }
 
     // VALID_MODES
@@ -199,49 +184,92 @@ public class DiffIds /*implements HasDescription*/ {
     }
     boolean isNormalReport = (null==mode) || mode.equals( MODE_REPORT );
 
+    // File IO
+    String outputFile = cmd.getOptionValue( "output_file" );
+    String encodingStr = cmd.getOptionValue( "encoding" );
+    if ( null==encodingStr || encodingStr.trim().length()<1 ) {
+      encodingStr = "UTF-8";
+    }
+    boolean strictEncoding = true;
+    if(cmd.hasOption("use_loose_encoding")) {
+      strictEncoding = false;
+    }
+
+    // Setup IO encoding
+    Charset charset = Charset.forName( encodingStr );
+    // Input uses Decoder
+    CharsetDecoder decoder = charset.newDecoder();
+    // Output uses Encoder
+    CharsetEncoder encoder = charset.newEncoder();
+    if ( strictEncoding ) {
+      decoder.onMalformedInput( CodingErrorAction.REPORT );
+      encoder.onMalformedInput( CodingErrorAction.REPORT );
+    }
+
+    PrintWriter out = null;
+    if( null!=outputFile && ! outputFile.equals("-") ) {
+        out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(outputFile), encoder), true);
+    } else {
+        out = new PrintWriter(new OutputStreamWriter(System.out, encoder), true);
+    }
+
     // Init
 	// HttpSolrServer solrA = new HttpSolrServer( URL1 );
-    HttpSolrServer solrA;
-    if ( null!=fullUrlA ) {
-      solrA = SolrUtils.getServer( fullUrlA );
+    HttpSolrServer solrA = null;
+    if ( null==fileA ) {
+      if ( null!=fullUrlA ) {
+        solrA = SolrUtils.getServer( fullUrlA );
+      }
+      else {
+        // Utils handle null values
+        solrA = SolrUtils.getServer( hostA, portA, collA );    
+      }
+      if(isNormalReport) out.println( "First Solr / Solr A = " + solrA.getBaseURL() );
     }
     else {
-      // Utils handle null values
-      solrA = SolrUtils.getServer( hostA, portA, collA );    
+      if(isNormalReport) out.println( "First Solr / Solr A read from file = " + fileA );    	
     }
-    if(isNormalReport) System.out.println( "First Solr / Solr A = " + solrA.getBaseURL() );
 
     // HttpSolrServer solrB = new HttpSolrServer( URL2 );
-    HttpSolrServer solrB;
-    if ( null!=fullUrlB ) {
-      solrB = SolrUtils.getServer( fullUrlB );
+    HttpSolrServer solrB = null;
+    if ( null==fileB ) {
+      if ( null!=fullUrlB ) {
+        solrB = SolrUtils.getServer( fullUrlB );
+      }
+      else {
+        // Utils handle null values
+        solrB = SolrUtils.getServer( hostB, portB, collB );    
+      }
+      if(isNormalReport) out.println( "Second Solr / Solr B = " + solrB.getBaseURL() );
     }
     else {
-      // Utils handle null values
-      solrB = SolrUtils.getServer( hostB, portB, collB );    
+      if(isNormalReport) out.println( "Second Solr / Solr B read from file = " + fileB );    	
     }
-    if(isNormalReport) System.out.println( "Second Solr / Solr B = " + solrB.getBaseURL() );
 
-    Set<String> aOnly = idsAOnly_fromServers( solrA, solrB );
-    Set<String> bOnly = idsBOnly_fromServers( solrA, solrB );
+	Set<String> idsA = (null!=solrA) ? SolrUtils.getAllIds( solrA ) : readIdsFromFile( new File(fileA), decoder );
+	Set<String> idsB = (null!=solrB) ? SolrUtils.getAllIds( solrB ) : readIdsFromFile( new File(fileB), decoder );
  
     if ( isNormalReport ) {
-	  System.out.println( "A-only: " + aOnly );
-	  System.out.println( "B-only: " + bOnly );
+      // Use non-destructive here since we use the lists more than once
+      Set<String> aOnly = SetUtils.inAOnly_nonDestructive(idsA, idsB);
+      Set<String> bOnly = SetUtils.inBOnly_nonDestructive(idsA, idsB);
+	  out.println( "A-only: " + aOnly );
+	  out.println( "B-only: " + bOnly );
     }
     else {
       Set<String> ids = null;
       if ( mode.equals(MODE_A_ONLY) ) {
-    	ids = idsAOnly_fromServers( solrA, solrB );
+        // destructive OK here since we're just doing 1 calculation
+        ids = SetUtils.inAOnly_destructive( idsA, idsB );
       }
       else if ( mode.equals(MODE_B_ONLY) ) {
-      	ids = idsBOnly_fromServers( solrA, solrB );
+        ids = SetUtils.inBOnly_destructive( idsA, idsB );
       }
       else if ( mode.equals(MODE_INTERSECT) ) {
-      	ids = idsIntersection_fromServers( solrA, solrB );  
+      	ids = SetUtils.intersection_destructive( idsA, idsB );  
       }
       else if ( mode.equals(MODE_UNION) ) {
-      	ids = idsUnion_fromServers( solrA, solrB );	  
+      	ids = SetUtils.union_destructive( idsA, idsB );  
       }
       else {
     	// This should never happen.
@@ -251,26 +279,10 @@ public class DiffIds /*implements HasDescription*/ {
       
       // Print the results
       for ( String id : ids ) {
-    	System.out.println( id );
+    	out.println( id );
       }
     }
-    
+    out.close();
   }
-
-  static String HOST0 = "localhost";
-  static String PORT0 = "8983";
-  static String COLL0 = "demo_shard1_replica1";
-  static String URL0 = "http://" + HOST0 + ":" + PORT0 + "/solr/" + COLL0;
-	  // + "/select?q=*:*&rows=" + ROWS + "&fl=id&wt=json&indent=on"
-
-  static String HOST1 = "localhost";
-  static String PORT1 = "8984"; // "8983";
-  static String COLL1 = "collection1";
-  static String URL1 = "http://" + HOST1 + ":" + PORT1 + "/solr/" + COLL1;
-
-  static String HOST2 = "localhost";
-  static String PORT2 = "8985"; // "8983";
-  static String COLL2 = "collection1";
-  static String URL2 = "http://" + HOST1 + ":" + PORT2 + "/solr/" + COLL2;
 
 }
